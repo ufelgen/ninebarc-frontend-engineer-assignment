baf139c9734cca29dea83b25360c52ab7ca58388
ufelgen <felgenhauer.u@gmail.com>
2023-02-24 23:57:30 +0100

    add header, form and card styling

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home({ onUpdateSearchedBooks, searchedBooks }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Book Quest</main>
-      <form onSubmit={() => getBooks(event)}>
+      <StyledHeading>Book Quest</StyledHeading>
+      <StyledForm onSubmit={() => getBooks(event)}>
         <label htmlFor="searchTerm">Search for a book title or author</label>
         <input
           id="searchTerm"
@@ -35,21 +35,58 @@ export default function Home({ onUpdateSearchedBooks, searchedBooks }) {
         <button type="submit" aria-label="submit your search">
           search
         </button>
-        {searchedBooks.docs.map((book) => (
-          <SearchResult key={book.key}>
-            <p>author: {book.author_name}</p>
-            <h4>title: {book.title}</h4>
-          </SearchResult>
-        ))}
-      </form>
+      </StyledForm>
+      {searchedBooks.docs?.map((book) => (
+        <SearchResult key={book.key}>
+          <p>author: {book.author_name}</p>
+          <h4>title: {book.title}</h4>
+        </SearchResult>
+      ))}
     </>
   );
 }
 
 const SearchResult = styled.article`
-  border: 1px solid var(--darkmagenta);
+  border: 2px solid var(--darkmagenta);
   border-radius: 5px;
   margin: 0.5rem;
   padding: 0.25rem;
   background-color: var(--lightgrey);
+`;
+
+const StyledForm = styled.form`
+  border: 2px solid var(--lightgrey);
+  border-radius: 5px;
+  margin: 0.5rem;
+  padding: 0.5rem 0.25rem;
+  background-color: var(--darkmagenta);
+  color: white;
+  font-weight: bold;
+
+  input {
+    padding: 0.25rem;
+    height: 4vh;
+    width: 77%;
+  }
+
+  button {
+    padding: 0.25rem;
+    margin: 0.5rem;
+    height: 4vh;
+    background-color: var(--darkgrey);
+    color: white;
+    border-radius: 5px;
+  }
+`;
+
+const StyledHeading = styled.header`
+  background-color: var(--darkgrey);
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  font-weight: bold;
+  font-size: 2rem;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
 `;
