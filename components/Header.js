312ac181e870f24fb82fd5fb680531b0eb9bedb2
ufelgen <felgenhauer.u@gmail.com>
2023-02-26 18:21:41 +0100

    add book icon to heading

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
+import { GiBookshelf } from "react-icons/gi";
 
 export default function Header() {
-  return <StyledHeading>Book Quest</StyledHeading>;
+  return (
+    <StyledHeading>
+      <StyledIcon />
+      Book Quest
+    </StyledHeading>
+  );
 }
 
 const StyledHeading = styled.header`
@@ -14,4 +20,8 @@ const StyledHeading = styled.header`
   font-size: 2rem;
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
+`;
+
+const StyledIcon = styled(GiBookshelf)`
+  margin-right: 0.5rem;
 `;
