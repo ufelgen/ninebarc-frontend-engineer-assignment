38b5d694139f7abfb8fe89d152fbf75e36290a55
ufelgen <felgenhauer.u@gmail.com>
2023-02-26 19:00:39 +0100

    test for search form

---
+++ components/SearchForm.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import SearchForm from "./SearchForm";
+
+test("calls the book search function with form input on submit", async () => {
+  const user = userEvent.setup();
+  const handleUpdateCurrentSearchTerm = jest.fn();
+  const handleUpdateSearchedBooks = jest.fn();
+
+  render(
+    <SearchForm
+      onUpdateCurrentSearchTerm={handleUpdateCurrentSearchTerm}
+      onUpdateSearchedBooks={handleUpdateSearchedBooks}
+    />
+  );
+
+  const inputField = screen.getByLabelText("Search for a book title or author");
+  const submitButton = screen.getByRole("button", {
+    name: "submit your search",
+  });
+
+  await user.type(inputField, "the light fantastic");
+  await user.click(submitButton);
+  expect(getBooks).toHaveBeenCalled();
+  expect(handleUpdateCurrentSearchTerm).toHaveBeenCalledWith(
+    "the light fantastic"
+  );
+});
