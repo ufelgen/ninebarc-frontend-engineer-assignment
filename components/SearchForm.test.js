de834a1a6dfb77e0c302801274627a82aa4c2ca4
ufelgen <felgenhauer.u@gmail.com>
2023-02-27 18:02:16 +0100

    update SearchForm.test.js after redux implementation

--- components/SearchForm.test.js
+++ components/SearchForm.test.js
@@ -1,10 +1,27 @@
 import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import SearchForm from "./SearchForm";
 import store from "../redux/store";
 import { Provider } from "react-redux";
 
-test("calls the book search function with form input on submit", async () => {
+test("renders the correct form elements", async () => {
+  render(
+    <Provider store={store}>
+      <SearchForm />
+    </Provider>
+  );
+
+  const inputField = screen.getByLabelText("Search for a book title or author");
+  const submitButton = screen.getByRole("button", {
+    name: "submit your search",
+  });
+
+  expect(inputField).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+});
+
+test("resets the form after submit", async () => {
   const user = userEvent.setup();
 
   render(
@@ -21,5 +38,6 @@ test("calls the book search function with form input on submit", async () => {
   await user.type(inputField, "the light fantastic");
   await user.click(submitButton);
 
+  expect(inputField).toHaveValue("");
   //expect(setCurrentSearchTerm).toHaveBeenCalledWith("the light fantastic");
 });
